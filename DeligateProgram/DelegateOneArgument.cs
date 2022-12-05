0105424ce677cebfd34b6291acb3450511293606
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-05 12:35:39 +0530

    Add files via upload

---
+++ DeligateProgram/DelegateOneArgument.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateOneArgument
+{
+    class Program
+    {
+        public delegate void DelegateName(string name);
+
+        static void PrintName(string name)
+        {
+
+            Console.WriteLine(name);
+        }
+
+        static void Main(string[] args)
+        {
+            var delegateObj = new DelegateName(PrintName);
+
+            delegateObj.Invoke("glory");
+
+
+        }
+    }
+
+}
---
+++ DeligateProgram/DelegateTwoArgument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateTwoArgument
+{
+    class Program
+    {
+        public delegate void DelegateName(string firstName,string secondName );
+
+        static void PrintName(string firstName,string secondName)
+        {
+
+            Console.WriteLine(firstName,secondName);
+        }
+
+        static void Main(string[] args)
+        {
+            var delegateObj = new DelegateName(PrintName);
+
+            delegateObj.Invoke("glory","susan_raju");
+
+        }
+    }
+}
