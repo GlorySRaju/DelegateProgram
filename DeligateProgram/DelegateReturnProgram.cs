fee2d2ed602c2000c7ebd1a6ec339868cfb6adb8
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-05 14:14:56 +0530

    Add files via upload

---
+++ DeligateProgram/DelegateReturnProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DeligateReturnProgram
+{
+    class Program
+    {
+        public delegate string DelegateName();
+        static string Name { get; set; }
+        
+        static string PrintName()
+        {
+            
+            return Name="glory";
+        }
+        
+        static void Main(string[] args)
+        {
+            var delegateObj = new DelegateName(PrintName);
+            string name=delegateObj.Invoke();
+
+            Console.WriteLine(name);
+        }
+    }
+}
