a84cb65c00173c16c5a3350e36a4491b2829b8b5
GlorySRaju <119593854+GlorySRaju@users.noreply.github.com>
2022-12-05 14:05:14 +0530

    Add files via upload

---
+++ DelegateTwoArgInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateMoreArgInt
+{
+    class Program
+    {
+        public delegate int Calculator(int firstNumber, int secondNumber);
+
+        static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+       
+        static void Main(string[] args)
+        {
+
+            var add = new Calculator(Add);
+            int sum = add.Invoke(5, 10);
+            Console.WriteLine(sum);
+  
+        }
+    }
+}
