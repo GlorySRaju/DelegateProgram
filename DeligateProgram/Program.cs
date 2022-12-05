35edd313e74549f582bbe4c20d187b24c8749b20
glory.sr <glory.sr@PIA3249>
2022-12-05 11:18:57 +0530

    Add project files.

---
+++ DeligateProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DeligateProgram
+{
+   
+    class Program
+    {
+        public delegate void DelegateRef();
+        static void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+
+            var delegateObj = new DelegateRef(Print);
+            delegateObj();
+        }
+ 
+    }
+}
+
